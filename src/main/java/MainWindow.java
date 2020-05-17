f5e364984d133647bafb29f727988232bddf4d11
Marat <eshmakar@mail.ru>
2020-05-17 15:33:47 +0300

    Был добавлен Singleton

---
+++ src/main/java/MainWindow.java
@@ -0,0 +1,16 @@
+//это Singleton
+public class MainWindow {
+    private MainWindow() {} //приватный конструктор, чтобы другие классы не смогли создать объект этого класса
+
+    private static class MainWindowHolder { //приватный вложенный класс, который создает объект класса Singleton
+        static MainWindow instance = new MainWindow();
+    }
+
+    public static MainWindow getInstance() { //можно будет вызвать объект этого класса через getInstance
+        return MainWindowHolder.instance;
+    }
+
+    public void show() {
+        System.out.println("Show main window");
+    }
+}
