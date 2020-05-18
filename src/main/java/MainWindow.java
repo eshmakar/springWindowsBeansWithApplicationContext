6095e9d248efc6490d314562c4b57c30195049f4
Marat <eshmakar@mail.ru>
2020-05-18 12:18:17 +0300

    implements InitializingBean(инит-метод), DisposableBean(дестрой-метод)
    с помощью этих интерфейсов можно обойтись без init-method и destroy-method которые содержатся в applicationContext.xml

--- src/main/java/MainWindow.java
+++ src/main/java/MainWindow.java
@@ -1,6 +1,17 @@
+import org.springframework.beans.factory.DisposableBean;
+import org.springframework.beans.factory.InitializingBean;
+
 //это Singleton
-public class MainWindow {
+public class MainWindow implements InitializingBean, DisposableBean {
     private MainWindow() {} //приватный конструктор, чтобы другие классы не смогли создать объект этого класса
+
+    public void destroy() throws Exception { //это метод интерфейса DisposableBean
+       closeConnection();
+    }
+
+    public void afterPropertiesSet() throws Exception { //это метод интерфейса InitializingBean
+        openConnection();
+    }
 
     private static class MainWindowHolder { //приватный вложенный класс, который создает объект класса Singleton
         static MainWindow instance = new MainWindow();
@@ -14,7 +25,11 @@ public class MainWindow {
         System.out.println("Show main window");
     }
 
-    public void connection(){
-        System.out.println("Main window connection");
+    public void openConnection(){
+        System.out.println("Main window open connection");
+    }
+
+    public void closeConnection(){
+        System.out.println("Main window close connection");
     }
 }
